44343d4c86c5e256b60bd6b3f4a49ea8dc67491e
rafliesa <rflisaa@gmail.com>
2025-04-02 17:53:42 +0700

    test deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,13 +25,13 @@ import { CommunityMember } from './community/entity/community.member.entity';
           useFactory: (configService: ConfigService) => ({
             type: 'postgres',
             host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
+            port: parseInt(process.env.DB_PORT || '5432', 10), // Konversi ke number
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
             entities: [User, Post, Like, Comment, Community, CommunityMember],
             synchronize: true,
-            ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+            ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
           }),
           inject: [ConfigService]
         }),
